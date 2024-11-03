c2dcbe9b7915b9e24bc76550ca6b91d63ba405c6
shuyshuys <ivlyns@protonmail.com>
2024-11-03 09:28:50 +0700

    rRefactor: sidebar links to update URLs

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -3,48 +3,48 @@
         <nav class="iq-sidebar-menu">
             <ul id="iq-sidebar-toggle" class="iq-menu">
                 <li class="">
-                    <a href="/dashboard" class=" ">
-                        <i class="ri-settings-line"></i></i><span>Administrator</span>
+                    <a href="/home" class=" ">
+                        <i class="ri-home-line"></i></i><span>Home</span>
                     </a>
                 </li>
                 <hr>
                 <li class="">
-                    <a href="/profile" class=" ">
+                    <a href="/kriteria" class=" ">
                         <i class="ri-survey-line"></i><span>Kriteria</span>
                     </a>
                 </li>
                 <li class="">
-                    <a href="/favorite" class=" ">
+                    <a href="/alternatif" class=" ">
                         <i class="ri-lightbulb-line"></i><span>Alternatif</span>
                     </a>
                 </li>
                 <li class="">
-                    <a href="/" class=" ">
+                    <a href="/perbandingan-kriteria" class=" ">
                         <i class="ri-scales-line"></i></i><span>Perbandingan Kriteria</span>
                     </a>
                 </li>
                 <li class="">
-                    <a href="/tickets" class=" ">
+                    <a href="/matriks-keputusan" class=" ">
                         <i class="ri-layout-grid-line"></i><span>Matriks Keputusan</span>
                     </a>
                 </li>
                 <li class="">
-                    <a href="/video" class=" ">
+                    <a href="/normalisasi" class=" ">
                         <i class="ri-folder-chart-line"></i><span>Normalisasi</span>
                     </a>
                 </li>
                 <li class="">
-                    <a href="/newspaper" class=" ">
+                    <a href="/matriks-rata-rata" class=" ">
                         <i class="ri-calculator-line"></i><span>Matriks Rata Rata</span>
                     </a>
                 </li>
                 <li class="">
-                    <a href="/chat" class=" ">
+                    <a href="/konsistensi" class=" ">
                         <i class="ri-file-lock-line"></i><span>Konsistensi</span>
                     </a>
                 </li>
                 <li class="">
-                    <a href="/game" class=" ">
+                    <a href="/hasil" class=" ">
                         <i class="ri-checkbox-line"></i></i><span>Hasil</span>
                     </a>
                 </li>
