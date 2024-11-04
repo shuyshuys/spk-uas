c1ad586482c8053c02455db5f1b3644992b55b51
shuyshuys <ivlyns@protonmail.com>
2024-11-04 23:04:58 +0700

    refactor: reorder and categorize label

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -2,12 +2,33 @@
     <div id="sidebar-scrollbar">
         <nav class="iq-sidebar-menu">
             <ul id="iq-sidebar-toggle" class="iq-menu">
+                <li class="mt-3">
+                    <label for="input-user">Web</label>
+                </li>
                 <li class="">
                     <a href="/home" class=" ">
                         <i class="ri-home-line"></i></i><span>Home</span>
                     </a>
                 </li>
                 <hr>
+                {{-- text "Input User" --}}
+                <li class="">
+                    <label for="input-user">Input User</label>
+                </li>
+                <li class="">
+                    <a href="/perbandingan-kriteria" class=" ">
+                        <i class="ri-scales-line"></i></i><span>Perbandingan Kriteria</span>
+                    </a>
+                </li>
+                <li class="">
+                    <a href="/saw" class=" ">
+                        <i class="ri-scales-line"></i></i><span>Simple Additive Weighting</span>
+                    </a>
+                </li>
+                <hr>
+                <li class="">
+                    <label for="input-user">Data</label>
+                </li>
                 <li class="">
                     <a href="/kriteria" class=" ">
                         <i class="ri-survey-line"></i><span>Kriteria</span>
@@ -19,15 +40,19 @@
                     </a>
                 </li>
                 <li class="">
-                    <a href="/perbandingan-kriteria" class=" ">
-                        <i class="ri-scales-line"></i></i><span>Perbandingan Kriteria</span>
+                    <a href="/konsistensi" class=" ">
+                        <i class="ri-file-lock-line"></i><span>Konsistensi</span>
                     </a>
                 </li>
+                <hr>
                 <li class="">
-                    <a href="/matriks-keputusan" class=" ">
-                        <i class="ri-layout-grid-line"></i><span>Matriks Keputusan</span>
-                    </a>
+                    <label for="input-user">Output</label>
                 </li>
+                {{-- <li class="">
+                    <a href="/Normalisasi" class=" ">
+                        <i class="ri-layout-grid-line"></i><span>Normalisasi</span>
+                    </a>
+                </li> --}}
                 <li class="">
                     <a href="/normalisasi" class=" ">
                         <i class="ri-folder-chart-line"></i><span>Normalisasi</span>
@@ -36,11 +61,6 @@
                 <li class="">
                     <a href="/matriks-rata-rata" class=" ">
                         <i class="ri-calculator-line"></i><span>Matriks Rata Rata</span>
-                    </a>
-                </li>
-                <li class="">
-                    <a href="/konsistensi" class=" ">
-                        <i class="ri-file-lock-line"></i><span>Konsistensi</span>
                     </a>
                 </li>
                 <li class="">
