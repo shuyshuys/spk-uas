37578fb160c8cc724ab2f4ce5569ff0385b998dd
shuyshuys <ivlyns@protonmail.com>
2024-11-04 23:08:54 +0700

    Refactor NormalisasiController to include update method for MatriksKeputusan

--- app/Http/Controllers/NormalisasiController.php
+++ app/Http/Controllers/NormalisasiController.php
@@ -2,15 +2,39 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\KonsistensiRasio;
+use App\Models\Hasil;
+use App\Models\MatriksKeputusan;
+use App\Models\PerbandinganKriteria;
 use Illuminate\Http\Request;
 
 class NormalisasiController extends Controller
 {
     public function index()
     {
-        $konsistensis = KonsistensiRasio::all();
+        $matriks = MatriksKeputusan::with(['kriteria'])->get();
+        $hasils = Hasil::orderBy('id', 'desc')->limit(5)->get();
 
-        return view('pages.normalisasi', compact('konsistensis'));
+        // Group by kriteria_id and then by alternatif_id
+        $matriksGrouped = $matriks->groupBy('kriteria_id');
+
+        $perbandingans = PerbandinganKriteria::all();
+        $perbandingansGrouped = $perbandingans->groupBy('kriteria2_id');
+
+        return view('pages.matriks-keputusan', compact('matriksGrouped', 'hasils', 'perbandingansGrouped'));
+    }
+
+    public function update(Request $request)
+    {
+        $updates = $request->input('updates');
+
+        foreach ($updates as $update) {
+            $matriksKeputusan = MatriksKeputusan::find($update['id']);
+            if ($matriksKeputusan) {
+                $matriksKeputusan->nilai = $update['nilai'];
+                $matriksKeputusan->save();
+            }
+        }
+
+        return response()->json(['success' => true]);
     }
 }
