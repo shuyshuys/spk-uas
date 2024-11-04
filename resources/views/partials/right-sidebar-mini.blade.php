227cc9d18abb48019a36c2bc857fbd3a11e900fb
shuyshuys <ivlyns@protonmail.com>
2024-11-04 15:41:01 +0700

    Update right-sidebar-mini.blade.php to display the latest KonsistensiRasio data in the sidebar

--- resources/views/partials/right-sidebar-mini.blade.php
+++ resources/views/partials/right-sidebar-mini.blade.php
@@ -4,21 +4,23 @@
             <div class="card-body p-0">
                 <div class="media-height p-3" data-scrollbar="init">
                     <h4 class="pt-3">Hasil terbaru</h4>
-                    <div class="d-flex align-items-center">
-                    </div>
                     <hr style="border: 1px solid grey; margin: 6px 0;" class="mb-2">
-                    <div class="d-flex align-items-center mb-4">
-                        <div class="iq-profile-avatar status-online">
-                            <img class="rounded-circle avatar-50" src="../assets/images/logo.png" alt="">
+                    @foreach ($konsistensis as $konsistensi)
+                        <div class="d-flex align-items-start mb-4">
+                            <div class="iq-profile-avatar status-online">
+                                <img class="rounded-circle avatar-50"
+                                    src="{{ $konsistensi->kriteria->user->profile_picture }}" alt="">
+                            </div>
+                            <div class="ms-3">
+                                <h5>Konsistensi Rasio<br>{{ $konsistensi->kriteria->nama }}</h5>
+                                <h6 class="mb-0">t = 6.0579</h6>
+                                <h6 class="mb-0">CI = {{ $konsistensi->ci }}</h6>
+                                <h6 class="mb-0">CR = {{ $konsistensi->cr }}</h6>
+                                <h6 class="mb-0">Hasil = {{ $konsistensi->hasil }}</h6>
+                            </div>
                         </div>
-                        <div class="ms-3">
-                            <h5>Konsistensi Rasio</h5>
-                            <h6 class="mb-0">t = 6.0579</h6>
-                            <h6 class="mb-0">CI = 0.0116</h6>
-                            <h6 class="mb-0">CR = 0.0093</h6>
-                            <h6 class="mb-0">Hasil = Konsisten</h6>
-                        </div>
-                    </div>
+                        <hr style="border: 1px solid grey; margin: 6px 0;" class="mb-2">
+                    @endforeach
                 </div>
                 <div class="right-sidebar-toggle bg-primary text-white mt-3">
                     <i class="ri-arrow-left-line side-left-icon"></i>
