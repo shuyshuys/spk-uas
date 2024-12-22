f55a8796595333d70b906adc559f159e39f648b3
shuyshuys <ivlyns@protonmail.com>
2024-12-22 16:08:05 +0700

    refactor: update dashboard title and navigation label

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -7,9 +7,9 @@ use Illuminate\Contracts\Support\Htmlable;
 
 class Dashboard extends \Filament\Pages\Dashboard
 {
-    protected static ?string $title = 'Sistem Pendukung Keputusan Dashboard';
+    protected static ?string $title = 'SPK Analytical Hierarchy Process and Weighted Product Dashboard';
 
-    protected static ?string $navigationLabel = 'SPK Dashboard';
+    protected static ?string $navigationLabel = 'Dashboard';
 
     protected int | string | array $columnSpan = [
         'md' => 2,
